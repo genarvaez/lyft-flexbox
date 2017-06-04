9544addaf0ae4918cfa631395496dff6f9056325
genarvaez <ge_naravez@hotmail.com>
2017-06-04 14:11:14 -0400

    terminada seccion de video continuo con el footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function (e) {
         var menu = document.getElementById('menu');
         var logo = document.querySelector(".logo img")
         var signUp = document.getElementById("signUp");
-        if (document.documentElement.scrollTop || document.body.scrollTop > 200) {
+        if (document.documentElement.scrollTop || document.body.scrollTop > 50) {
                 menu.classList.add('white');
                 logo.removeAttribute("src");
                 logo.setAttribute("src", "assets/images/logo-pink.png");
