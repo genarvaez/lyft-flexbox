51e1360ef325dce07bafd5a77dba20bec916418a
genarvaez <ge_naravez@hotmail.com>
2017-06-05 00:43:47 -0400

    funcionalidad de despliegue de formulario, sin validación

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,48 @@
+var input = Array.from(document.getElementsByClassName("noShow"));
+var validate = Array.from(document.getElementsByTagName("input"));
+var show = Array.from(document.getElementsByClassName("show"));
+var phone = document.getElementById("phone");
+var name = document.getElementById("name");
+var email = document.getElementById("email");
+var city = document.getElementById("city");
+var signButton = document.getElementById("sign-button");
+
+//controla la barra al hacer scroll 
 window.addEventListener('scroll', function (e) {
         var menu = document.getElementById('menu');
-        var logo = document.querySelector(".logo img")
+        var logo = document.querySelector(".logo img");
         var signUp = document.getElementById("signUp");
         if (document.documentElement.scrollTop || document.body.scrollTop > 50) {
                 menu.classList.add('white');
                 logo.removeAttribute("src");
                 logo.setAttribute("src", "assets/images/logo-pink.png");
                 signUp.classList.remove("hide");
-        } 
+            }
         else{
         	menu.classList.remove('white');
         	logo.removeAttribute("src");
             logo.setAttribute("src", "assets/images/logo-white.png")
             signUp.classList.add("hide");
+        }//oculta el formulario si el usuario hace scroll hacia abajo
+        if(document.documentElement.scrollTop || document.body.scrollTop > 350){
+            input.forEach(function(e,i){
+                e.classList.add("noShow");
+                show.forEach(function(e){e.classList.add("show")});
+            });
         }
     });
-var register = document.getElementById("register");
-var inputPhone = document.getElementById("inputPhone");
-inputPhone.addEventListener("click", function(){
-  
+
+//despliega el formulario de registro
+phone.addEventListener("click", function(){
+    input.forEach(function(e){
+        e.classList.remove("noShow");
+    })
+
+})
+validate.forEach(function(e,i){
+    e.addEventListener("click", function(){
+        if(e.value == ""){
+            show[i].classList.remove("show");
+        }
+    })
 })
